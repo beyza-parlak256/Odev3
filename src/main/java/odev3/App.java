2dc1ed99869cd60c07e31fde09509833c940340d
beyza-parlak256 <beyza.parlak98@erzurum.edu.tr>
2022-05-05 22:33:25 +0300

    Heroku add

--- src/main/java/odev3/App.java
+++ src/main/java/odev3/App.java
@@ -25,24 +25,27 @@ public class App {
         Logger logger = (Logger) LogManager.getLogger(App.class);
         ((org.apache.logging.log4j.Logger) logger).error("hello world");
 
-        get("/", (req,res) -> "Hello, world!");
+        var port = Integer.parseInt(System.getenv("PORT"));
+        port(port);
+        ((org.apache.logging.log4j.Logger) logger).error("Current port number:" + port);
+
+        get("/", (req, res) -> "Hello, world!");
 
         get("/compute",
-        (rq,rs) -> {
-            Map<String,String> map = new HashMap<String,String>();
-            map.put("result", "not computed yet!");
-            return new ModelAndView(map, "compute.mustache");
-        },
-        new MustacheTemplateEngine()
-        );
+                (rq, rs) -> {
+                    Map<String, String> map = new HashMap<String, String>();
+                    map.put("result", "not computed yet!");
+                    return new ModelAndView(map, "compute.mustache");
+                },
+                new MustacheTemplateEngine());
 
-        post("/compute", (req, res) ->{
+        post("/compute", (req, res) -> {
             String input1 = req.queryParams("input1");
             java.util.Scanner sc1 = new java.util.Scanner(input1);
             sc1.useDelimiter("[;\r\n]+");
             java.util.ArrayList<Integer> inpuList = new java.util.ArrayList<>();
-            while(sc1.hasNext()){
-                int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+            while (sc1.hasNext()) {
+                int value = Integer.parseInt(sc1.next().replaceAll("\\s", ""));
                 inpuList.add(value);
             }
             sc1.close();
@@ -58,17 +61,21 @@ public class App {
             return new ModelAndView(map, "compute.mustache");
 
         },
-        new MustacheTemplateEngine()
-        );
+                new MustacheTemplateEngine());
+    }
+
+    private static void port(int port) {
     }
 
     public static boolean search(ArrayList<Integer> array, int e) {
         System.out.println("inside search");
-        if (array == null) return false;
-        
+        if (array == null)
+            return false;
+
         for (int elt : array) {
-        if (elt == e) return true;
+            if (elt == e)
+                return true;
         }
         return false;
-        }
+    }
 }
